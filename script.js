3b2cd0993e5ed132fd4748049efab75e89d585d8
Roberto Alagemovits <robertomovits@gmail.com>
2023-09-21 02:05:00 -0300

    agregue una matriz que representa el tablero y const de celulas vivas o muertas

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const divTablero = document.querySelector("#tablero");
 const MAX_FILA = 10;
 const MAX_COL = 10;
 
+const VIVA = true;
+const MUERTA = false;
+
+const matrizTablero = [];
+function initMatrizTablero() {
+    for (let fila = 0; fila < MAX_FILA; fila++) {
+        matrizTablero[fila] = [];
+        for (let columna = 0; columna < MAX_COL; columna++) {
+            matrizTablero[fila][columna] = MUERTA;
+        }
+    }
+}
+
 // Genera el Tablero de la pagina
 function generarMatriz() {
     divTablero.innerHTML = "";    
@@ -26,11 +39,3 @@ function agregarColumnas(fila) {
 }
 
 generarMatriz();
-
-
-
-
-/* // Verifica si la posicion esta dentro del tablero
-function limiteTablero(fila, col) {
-    return fila >= 0 && fila < MAX_FILA && col >= 0 && col < MAX_COL;
-} */
